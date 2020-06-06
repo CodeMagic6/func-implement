56cffae151b7f977917db878f0ed93e091b81cd2
wangkai <maxxwill@163.com>
2020-06-06 11:16:54 +0800

    co

--- co.js
+++ co.js
@@ -1,1 +1,30 @@
 // TODO co函数的实现
+
+function co(gen){
+    let it = gen();
+    return new Promise(function(resolve, reject){
+        !function next(lastValue){
+            let { value, done } = it.next(lastValue);
+            if(done){
+                resolve(value);
+            } else {
+                value.then(next, reason => reject(reason));
+            }
+        }();
+    });
+}
+
+// co函数关键，1，一个自执行的next函数。 2，
+function co(gen){
+    let it = gen();
+    return new Promise(function(resolve, reject){
+        !function next(lastValue){
+            let { value, done } = it.next(lastValue);
+            if(done){
+                resolve(value);
+            } else {
+                value.then(next, reason => reject(reason));
+            }
+        }();
+    });
+}
