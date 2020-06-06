0231503f06ee1891f6d5fe522e8c90d4f0b4f730
wangkai <maxxwill@163.com>
2020-06-06 16:42:31 +0800

    promise

--- q-bludbird.js
+++ q-bludbird.js
@@ -15,8 +15,8 @@ var defer = function () {
             }
         },
         promise: {
-            then: function (callback) { 
-                if(pending){
+            then: function (callback) {
+                if (pending) {
                     pending.push(callback);
                 } else {
                     callback(value);
@@ -28,12 +28,12 @@ var defer = function () {
 
 //bluebird
 module.exports = {
-    promisify(fn){
-        return function(){
+    promisify(fn) {
+        return function () {
             var args = Array.from(arguments);
-            return new Promise(function(resolve, reject){
-                fn.apply(null, args.concat(function(err){
-                    if(err){
+            return new Promise(function (resolve, reject) {
+                fn.apply(null, args.concat(function (err) {
+                    if (err) {
                         reject(err);
                     } else {
                         resolve(arguments[1]);
@@ -42,10 +42,135 @@ module.exports = {
             })
         }
     },
-    promisifyAll(obj){
-        for(let attr in obj){
-            if(obj.hasOwnProperty(attr) && typeof obj[attr] == 'function'){
-                obj[attr+'Async'] = this.promisify(obj[attr]);
+    promisifyAll(obj) {
+        for (let attr in obj) {
+            if (obj.hasOwnProperty(attr) && typeof obj[attr] == 'function') {
+                obj[attr + 'Async'] = this.promisify(obj[attr]);
+            }
+        }
+        return obj;
+    }
+}
+
+var defer = function () {
+    let pending = [], value;
+    return {
+        resolve: function (_value) {
+            value = _value;
+            for (let i = 0, len = pending.length; i < len; i++) {
+                let callback = pending[i];
+                callback(value);
+            }
+            pending = undefined;
+        },
+        promise: {
+            then: function (callback) {
+                if (pending) {
+                    pending.push(callback);
+                } else {
+                    callback(value);
+                }
+            }
+        }
+    }
+};
+
+
+module.exports = {
+    promisify(fn) {
+        return function () {
+            let args = Array.from(arguments);
+            return new Promise(function (resolve, reject) {
+                fn.apply(null, args.concat(function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(arguments[1]);
+                    }
+                }))
+            });
+        }
+    },
+    promisifyAll(obj) {
+        for (let attr in obj) {
+            if (obj.hasOwnProperty(attr) && typeof obj[attr] == 'function') {
+                obj[attr + 'Async'] = this.promisify(obj[attr]);
+            }
+        }
+        return obj;
+    }
+}
+
+
+let defer = function () {
+    let pending = [], value;
+    return {
+        promise: {
+            then: function (callback) {
+                if (pending) {
+                    pending.push(callback);
+                } else {
+                    callback(value)
+                }
+            }
+        },
+        resolve: function (_value) {
+            let value = _value;
+            if (pending) {
+                for (let i = 0, len = pending.length; i < len; i++) {
+                    let callback = pending[i];
+                    callback(value);
+                }
+                pending = undefined;
+            }
+        }
+    }
+}
+
+let defer = function () {
+    let pending = [], value;
+    return {
+        resolve: function (_value) {
+            let value = _value;
+            if (pending) {
+                for (let i = 0, len = pending.length; i < len; i++) {
+                    let callback = pending[i];
+                    callback(value);
+                }
+                pending = undefined;
+            }
+        },
+        promise: {
+            then: function (callback) {
+                if (pending) {
+                    pending.push(callback);
+                } else {
+                    callback(value);
+                }
+            }
+        }
+    }
+}
+
+module.exports = {
+    promisify(fn) {
+        return function () {
+            let args = Array.from(arguments);
+            return new Promise(function (resolve, resolve) {
+                fn.apply(null, args.concat(function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(arguments[1]);
+                    }
+                }))
+            });
+        }
+    },
+    promisifyAll(obj) {
+        for (let attr in obj) {
+            if (obj.hasOwnProperty(attr) && typeof obj[attr] == 'function') {
+                obj[attr + 'Async'] = this.promisify(obj[attr]);
             }
         }
         return obj;
