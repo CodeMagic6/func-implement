8d97e72ff4af755382dc3b0c728c627ba1c187f0
仁凯 <maxxwill@163.com>
2020-09-04 05:25:54 +0800

    Update combineReduces.js

--- combineReduces.js
+++ combineReduces.js
@@ -1,5 +1,12 @@
-export default function combineReducers(){
-  return function combination(){
-    
+export default function combineReducers(reducers){
+  return function combination(state = {}, action){
+    let nextState = {}
+    for(let key in reducers) {
+      let recuder = reducers[key];
+      nextState[key] = reducer(state[key], action);
+      hasChanged = hasChanged || nextState[key] !== state[key];
+    }
+    hasChanged = hasChanged || Object.keys(state).length !== Object.keys(nextState).length;
+    return hasChanged ? nextState: state;
   }
 }
